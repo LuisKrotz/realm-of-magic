585ef5750cd9294cd66dbdb53931dc1b68d16c82
LuisKrotz <luis.krotz@gmail.com>
2022-04-30 15:39:34 -0300

    CREATE LOGIC: setup firebase, create login, create routes , test API responses

---
+++ src/app/config.js
@@ -0,0 +1,16 @@
+module.exports = global.config = {
+    tokens: {
+        authToken: 'Auth Token'
+    },
+    routes: {
+        create: "/create",
+        login: "/login",
+        list: "/",
+        
+    },
+    api: {
+        path: "https://9488e748.us-south.apigw.appdomain.cloud/api/v1",
+        spells: "/spells",
+        findByID: "/spells/findById"
+    }
+}
---
+++ src/app/firebase.config.js
@@ -0,0 +1,15 @@
+// Vendor
+// ----------
+import { initializeApp }    from "firebase/app";
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyCXiNaoImfUAUHa2zi1Puaex4zrgmiNTXI",
+    authDomain: "magick-demo.firebaseapp.com",
+    projectId: "magick-demo",
+    storageBucket: "magick-demo.appspot.com",
+    messagingSenderId: "983508534470",
+    appId: "1:983508534470:web:322faf3c8beb780e6ec3c7"
+};
+
+export const app = initializeApp(firebaseConfig);
