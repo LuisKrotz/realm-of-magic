519d0edd1e9fae055195ed9129e353b8e7f96e34
LuisKrotz <luis.krotz@gmail.com>
2022-05-01 00:33:46 -0300

    LOGIN: add toastify to project, use in login instead of rendering array of errors (#16)

--- src/components/pages/Login.js
+++ src/components/pages/Login.js
@@ -1,16 +1,15 @@
 // eslint-disable-next-line no-unused-vars
 import { app } from '../../app/firebase.config'
 import { getAuth, signInWithEmailAndPassword, createUserWithEmailAndPassword }  from 'firebase/auth'
-
 import { useState, useEffect } from "react"
 import { useNavigate } from "react-router-dom"
+import { toast } from 'react-toastify'
 import Button from "../common/LoginButton"
 
 
 const LoginFields = () => {
     const [email, setEmail] = useState('')
     const [password, setPassword] = useState('')
-    const [errors, setErrors] = useState('')
 
     const navigate = useNavigate()
     let token = sessionStorage.getItem(global.config.tokens.authToken)
@@ -31,30 +30,28 @@ const LoginFields = () => {
 
         if (action === 'register')
           createUserWithEmailAndPassword(authentication, email, password)
-          .then((response) => login(response._tokenResponse.refreshToken))
+          .then((response) => {
+              login(response._tokenResponse.refreshToken)
+
+              toast.success('Welcome to Magic Demo!')
+            })
           .catch((error) => {
-                let array = [];
-
-                console.log(error)
-
-                if (error.code === 'auth/email-already-in-use') array.push('Email in use, please sign in!')
-                if (error.code === 'auth/auth/invalid-email') array.push('Please, provide a valid email.')
-                if (error.code === 'auth/weak-password') array.push('Please, provide a stronger password.')
-                if (error.code === 'auth/internal-error') array.push('An unexpected error occoured, please reload the page and try again.')
-
-                setErrors(array);
+                if (error.code === 'auth/email-already-in-use') toast.info('Email in use, please sign in!')
+                if (error.code === 'auth/auth/invalid-email') toast.error('Please, provide a valid email.')
+                if (error.code === 'auth/weak-password') toast.error('Please, provide a stronger password.')
+                if (error.code === 'auth/internal-error') toast.error('An unexpected error occoured, please reload the page and try again.')
           })
         else
             signInWithEmailAndPassword(authentication, email, password)
-            .then((response) => login(response._tokenResponse.refreshToken))
+            .then((response) => {
+                login(response._tokenResponse.refreshToken)
+
+                toast.success('Welcome Back!')
+            })
             .catch((error) => {
-                let array = [];
-
-                if (error.code === 'auth/wrong-password') array.push('Please check the Password.')
-                if (error.code === 'auth/user-not-found') array.push('Please check the Email.')
-                if (error.code === 'auth/internal-error') array.push('An unexpected error occoured, please reload the page and try again.')
-
-                setErrors(array);
+                if (error.code === 'auth/wrong-password') toast.warn('Please check the Password.')
+                if (error.code === 'auth/user-not-found') toast.warn('Please check the Email.')
+                if (error.code === 'auth/internal-error') toast.error('An unexpected error occoured, please reload the page and try again.')
             })
       }
 
@@ -86,15 +83,6 @@ const LoginFields = () => {
                     handleAction={() => handleAction('login')} />
             <Button label={'Sign Up'}
                     handleAction={() => handleAction('register')} />
-
-
-            { errors.length > 0 &&
-                <ul>
-                    { errors.map((error, key) => (
-                        <li key={key}>{error}</li>
-                    ))}
-                </ul>
-            }
         </form>
     )
 }
